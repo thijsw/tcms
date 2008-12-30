30ef169d2151fad34031e3f8790ae0949540c56a
Thijs Wijnmaalen <thijs@wijnmaalen.name>
2008-12-30 23:29:30 +0100

    file download from library module

--- modules/library/frontend.php
+++ modules/library/frontend.php
@@ -2,6 +2,12 @@
 
 class Frontend_Library extends Frontend {
 
+	protected $data;
+
+	public function index () {
+		return STATUS_NOT_FOUND;
+	}
+
 	public function download () {
 		if ($this->get(1) < 1) return STATUS_NOT_FOUND;
 		$this->id = (int) $this->get(1);
@@ -18,6 +24,18 @@ class Frontend_Library extends Frontend {
 			throw new Exception_Core("File $path could not be found on disk");
 		}
 
+		$res = Response::getInstance();
+		$res->set_content_type($file->mimetype);
+		$res->set_header('Content-Length', $file->size);
+		$res->set_header('Content-Disposition', sprintf('attachment; filename="%s"', $file->name));
+
+		$this->data = file_get_contents($path);
+
+		return STATUS_OK;
+	}
+
+	public function render () {
+		return $this->data;
 	}
 
 }
